dc8e4ef8ec2b7aba0c20aec98ee926e83d304560
Aru Bhardwaj <aru.bhardwaj01@gmail.com>
2021-06-12 22:02:10 +0530

    typo

--- R/data.R
+++ R/data.R
@@ -2,13 +2,13 @@
 #' This dataset contains data on test performance,
 #' school characteristics and student demographic backgrounds. The data used here are from all 420 K-6 and K-8 districts in
 #' California with data available for 1998 and 1999
-#' @format {
-#' \item{observation_n~r}{float, %9.0g}
-#' \item{dist_cod}{float, %9.0g}
-#' \item{county}{str18, %18s}
-#' \item{district}{str53, %18s}
-#' ...
 #'
+#'  @format {
+#'  \item{observation_n~r}{float, %9.0g}
+#'  \item{dist_cod}{float, %9.0g}
+#'  \item{county}{str18, %18s}
+#'  \item{district}{str53, %18s}
+#' ...
 #' }
 #'
 #'
